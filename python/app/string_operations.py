6a6662d530a67412aee2479b3bfb5282ce677b45
Allan Arceno <arcenoallan214@gmail.com>
2025-03-05 13:11:37 +0800

    2025-03-05: bootstrap sonarqube

--- python/app/string_operations.py
+++
@@ -1,5 +0,0 @@
-def to_uppercase(s):
-    return s.upper()
-#
-#def to_lowercase(s):
-#    return s.lower()
--- python/test/test_string_operations.py
+++
@@ -1,9 +0,0 @@
-#import unittest
-#from app.string_operations import to_uppercase#, to_lowercase
-#
-#class TestStringOperations(unittest.TestCase):
-#    def test_to_uppercase(self):
-#        self.assertEqual(to_uppercase('hello'), 'HELLO')
-#
-#    def test_to_lowercase(self):
-#        self.assertEqual(to_lowercase('HELLO'), 'hello')
